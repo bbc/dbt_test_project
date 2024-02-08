3bee07985418cf5721150e9a706194efed0559b5
bertie_shaw <bertie.b.shaw@bbc.co.uk>
2024-02-08 13:51:27 +0000

    moved raw_data file inside dbt directory

--- dbt/models/marts/.ipynb_checkpoints/age_splits-checkpoint.sql
+++ dbt/models/marts/.ipynb_checkpoints/age_splits-checkpoint.sql
@@ -1,16 +1,10 @@
--- models/marts/brand_engagement_metrics.sql
-
-{{ config(
-    materialized='table',
-    schema='central_analytics_sandbox'
-    model_name='bs_dbt_test_project_age_split'
-) }}
+-- models/marts/age_splits.sql
 
     with topid as (
         select 
             brand_id
         from
-            {{ ref('bs_dbt_test_project_raw') }}
+            {{ ref('load_raw_data') }}
         where date_of_event = ('<params.run_date')::date - 1
         group by brand_id
         order by count(distinct audience_id) desc
@@ -21,6 +15,6 @@ select
     age_range,
     count(distinct audience_id) as users
 from
-    {{ ref('bs_dbt_test_project_raw') }}
+    {{ ref('load_raw_data') }}
 where date_of_event = ('<params.run_date')::date - 1
 group by age_range
--- dbt/models/marts/aggregations.sql
+++ dbt/models/marts/aggregations.sql
@@ -1,11 +1,5 @@
 -- models/marts/brand_engagement_metrics.sql
 
-{{ config(
-    materialized='table',
-    schema='central_analytics_sandbox'
-    model_name='bs_dbt_test_project_aggregated'
-) }}
-
 select
     brand_id,
     brand_title,
@@ -13,7 +7,7 @@ select
     count(distinct audience_id) as users,
     sum(playback_time_total) as time_spent
 from
-    {{ ref('bs_dbt_test_project_raw') }}
+    {{ ref('load_raw_data') }}
 where date_of_event = ('<params.run_date')::date - 1
 group by
     brand_id,
